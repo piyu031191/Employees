480bf32bfd1a0332587a368a3b0a7062ace48df2
piyu031191 <priyankasambatkar0864@gmail.com>
2021-11-02 18:44:39 +0530

    add Partime Program

--- src/com/Company/Employee.java
+++ src/com/Company/Employee.java
@@ -4,11 +4,22 @@ public class Employee {
 
 	public static void main(String[] args) {
 		double present =Math.floor(Math.random()*2);
+		double partTime =Math.floor(Math.random()*2);
+		int wages; 
+
 		if(present == 1) {
-     	   System.out.println("Employee is present");
+     	   if(partTime == 1) {
+	
+			wages=160;
+	   		System.out.println("Employee is present & PartTime  with wages = " + wages);
+     	   }
+     	   else {
+	    	wages=160;
+	   		System.out.println("Employee is present & FullTime  with wages = " + wages);
+     	   }
 		} 
 		else {
-     	   System.out.println("Employee is absent");
-		}
+	         wages=0;
+	   		System.out.println("Employee is absent with wages = " + wages);		}
 	}
 }
